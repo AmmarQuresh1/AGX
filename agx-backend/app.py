acb2deceb8aad08b07407ba101ffdabb972bb14c
AmmarQuresh1 <mianammarqureshi@gmail.com>
2025-06-05 20:40:49 +0100

    add rate limiting (slowapi, redis)

--- agx-backend/app.py
+++ agx-backend/app.py
@@ -1,24 +1,42 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import FileResponse, JSONResponse
 from pydantic import BaseModel
 from agx.core import agx_main 
 from fastapi.middleware.cors import CORSMiddleware
+# Slowapi for rate limiting
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from redis import Redis
 
 app = FastAPI()
 
+# Allows requests from frontend on different port
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Or "*" to allow all
+    allow_origins=["http://localhost:3000", "http://192.168.1.97:3000"],  # Or "*" to allow all
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Plan downloading
 class Script(BaseModel):
     prompt: str
 
+# Rate limiting
+limiter = Limiter(key_func=get_remote_address,
+                  storage_uri="redis://localhost:6379")
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+@app.options("/")
+async def options_root():
+    return JSONResponse(content={"ok": True})
+
 @app.post("/", response_class=FileResponse)
-async def generate_script(script: Script):
+@limiter.limit("5/day")
+async def generate_script(script: Script, request: Request):
     plan = agx_main(script.prompt)
     if not plan:
         raise HTTPException(status_code=500, detail="Plan generation failed.")
